55f8fff61a0e85ac87c4a825bb7ddaa6c1754b26
wesleytruong <wesleytruong@ucsb.edu>
2024-03-07 00:00:19 -0800

    added some keymaps to remap.lua

--- lua/fruitninjaking/remap.lua
+++ lua/fruitninjaking/remap.lua
@@ -1,2 +1,11 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+function()
+-- when in highlight mode, can move text around with J and K
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- lets you yank into clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
