b4dac51597d47c5656038ff9f03d948041f3eb65
wesleytruong <wesleytruong@ucsb.edu>
2024-03-07 02:09:01 -0800

    added lsp supper with lsp zero

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ https://github.com/wbthomason/packer.nvim
 
 # ripgrep
 https://github.com/BurntSushi/ripgrep?tab=readme-ov-file#installation
+
+# mason
+for mason to work install
+    sudo apt install zip
+    sudo apt install unzip
+
+
--- lua/fruitninjaking/packer.lua
+++ lua/fruitninjaking/packer.lua
@@ -30,4 +30,22 @@ return require('packer').startup(function(use)
     requires = { {"nvim-lua/plenary.nvim"} }
   }
   use('tpope/vim-fugitive')
+
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    requires = {
+      --- Uncomment the two plugins below if you want to manage the language servers from neovim
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'L3MON4D3/LuaSnip'},
+    }
+  }
+
 end)
