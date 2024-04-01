07cc2d9cbe2d8ae1421cb4bd442b1b6aadc8e458
wesleytruong <wesleytruong@ucsb.edu>
2024-03-31 21:00:28 -0700

    added possible scheming plugin for easy colorscheme switching

---
+++ lua/fruitninjaking/lazy/colors/scheming.lua
@@ -0,0 +1,15 @@
+return {}
+--[[return {
+  "wllfaria/scheming.nvim",
+  --lazy = true,
+  opts = {
+    schemes = {
+      "tokyonight",
+      "melange",
+      "rose-pine",
+      "catppuccin",
+      "bamboo",
+    }
+  }
+}
+--]]
