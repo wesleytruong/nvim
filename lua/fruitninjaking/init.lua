1fbcaaac52baec8bff2b0cb0866c47efbe32810d
wesleytruong <wesleytruong@ucsb.edu>
2024-04-03 23:47:01 -0700

    added yank to clipboard support on wsl in init.lua file

--- lua/fruitninjaking/init.lua
+++ lua/fruitninjaking/init.lua
@@ -4,3 +4,14 @@ require("fruitninjaking.set")
 require("fruitninjaking.lazy_init")
 
 require("fruitninjaking.colors")
+
+-- Adds yank to clipboard support on wsl
+vim.opt.clipboard = "unnamedplus"
+if vim.fn.has('wsl') == 1 then
+  vim.api.nvim_create_autocmd('TextYankPost', {
+    group = vim.api.nvim_create_augroup('Yank', { clear = true }),
+    callback = function()
+      vim.fn.system('clip.exe', vim.fn.getreg('"'))
+    end,
+  })
+end
