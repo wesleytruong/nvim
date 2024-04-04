545b0a5a4d1c875910124af67f52e428686a5ed3
wesleytruong <wesleytruong@ucsb.edu>
2024-04-04 11:29:34 -0700

    removed legacy comment for wsl cp

--- lua/fruitninjaking/init.lua
+++ lua/fruitninjaking/init.lua
@@ -6,18 +6,6 @@ require("fruitninjaking.lazy_init")
 require("fruitninjaking.colors")
 
 -- Adds yank to clipboard support on wsl
---[[
-vim.opt.clipboard = "unnamedplus"
-if vim.fn.has('wsl') == 1 then
-  vim.api.nvim_create_autocmd('TextYankPost', {
-    group = vim.api.nvim_create_augroup('Yank', { clear = true }),
-    callback = function()
-      vim.fn.system('clip.exe', vim.fn.getreg('"'))
-    end,
-  })
-end
---]]
-
 if vim.fn.has('wsl') == 1 then
   vim.g.clipboard = {
     name = 'WslClipboard',
