54fffa39fa9957ba21344cc518b2a3e217593a1d
wesleytruong <wesleytruong@ucsb.edu>
2024-05-02 12:52:09 -0700

    added comment plugin

---
+++ lua/fruitninjaking/lazy/comment.lua
@@ -0,0 +1,16 @@
+return {
+  "numToStr/Comment.nvim",
+  opts = {
+    toggler = {
+      line = ';;'
+    },
+    opleader = {
+      line = ';'
+    },
+    extra = {
+      above = ';O',
+      below = ';o',
+      eol = ';A',
+    }
+  }
+}
