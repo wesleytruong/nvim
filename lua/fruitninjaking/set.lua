535036161b3f13c01397e9926229c8d09feed42f
wesleytruong <wesleytruong@ucsb.edu>
2024-05-05 17:42:56 -0700

    added line wrapping to all files

--- lua/fruitninjaking/set.lua
+++ lua/fruitninjaking/set.lua
@@ -30,3 +30,9 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 
+vim.opt.wrap = true
+-- enable indentation
+vim.opt.breakindent = true
+-- indent by an additional 2 characters on wrapped lines, when line >= 40 characters, put 'showbreak' at start of line
+vim.opt.breakindentopt = "shift:2,min:40,sbr"
+-- vim.opt.showbreak = ">>"
