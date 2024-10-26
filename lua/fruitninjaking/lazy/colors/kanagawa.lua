cca814754796d328fedc0c42cb1c9abe3c93dd27
wesleytruong <wesleytruong@ucsb.edu>
2024-10-25 19:51:51 -0700

    added some more colors

--- lua/fruitninjaking/lazy/colors/kanagawa.lua
+++ lua/fruitninjaking/lazy/colors/kanagawa.lua
@@ -1,16 +1,23 @@
 return {
-  "rebelot/kanagawa.nvim",
-  name = "kanagawa",
-  lazy = true,
-  opts = {
-    colors = {
-      theme = {
-        all = {
-          ui = {
-            bg_gutter = "none"
+  {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    lazy = true,
+    opts = {
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = "none"
+            }
           }
         }
       }
     }
+  },
+  {
+    "sho-87/kanagawa-paper.nvim",
+    name = "kanagawa-paper",
+    lazy = true
   }
 }
