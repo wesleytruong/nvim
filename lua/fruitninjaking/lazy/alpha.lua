34e8afcb35f259129ae3a769c81172fe4531df80
wesleytruong <wesleytruong@ucsb.edu>
2024-11-06 02:35:01 -0800

    added cool asciis and modified alpha dashboard

--- lua/fruitninjaking/lazy/alpha.lua
+++ lua/fruitninjaking/lazy/alpha.lua
@@ -5,6 +5,22 @@ return {
         'nvim-lua/plenary.nvim'
     },
     config = function ()
-        require'alpha'.setup(require'alpha.themes.theta'.config)
+      -- require'alpha'.setup(require'alpha.themes.theta'.config)
+      local dashboard = require("alpha.themes.dashboard")
+
+      local ascii = require('fruitninjaking.lazy.ascii.ascii')
+
+      dashboard.section.header.val = ascii['hydra']
+
+      dashboard.section.buttons.val = {
+        dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+        dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+        dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+        dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+        dashboard.button("c", "  Configuration", ":cd ~/.config/nvim | Explore<CR>"),
+        dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+      }
+
+      require('alpha').setup(dashboard.opts)
     end
 };
