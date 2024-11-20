70b47283f4df1321c5cd9c58e3cdf0afa9fc084c
wesleytruong <wesleytruong@ucsb.edu>
2024-11-20 01:01:23 -0800

    added inside file search to telescope

--- lua/fruitninjaking/lazy/telescope.lua
+++ lua/fruitninjaking/lazy/telescope.lua
@@ -2,11 +2,11 @@ return {
   {
     "nvim-telescope/telescope.nvim",
 
-    tag = "0.1.5",
+    tag = "0.1.8",
 
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "andrew-george/telescope-themes",
+      -- "andrew-george/telescope-themes",
       {
         'nvim-telescope/telescope-fzf-native.nvim',
         build = 'make',
@@ -17,15 +17,23 @@ return {
     },
 
     config = function()
-      require('telescope').setup({})
-
       local builtin = require('telescope.builtin')
+      -- vim.keymap.set('n', '<leader>cc', function()
+      --   require("lazy").load('*')
+      --   builtin.colorscheme({ enable_preview = true })
+      -- end)
       vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
       vim.keymap.set('n', '<c-p>', builtin.git_files, {})
       vim.keymap.set('n', '<leader>pws', function()
         local word = vim.fn.expand("<cword>")
         builtin.grep_string({ search = word })
       end)
+      vim.keymap.set('n', '<leader>fs', require('telescope.builtin').current_buffer_fuzzy_find, {})
+      -- vim.keymap.set('n', '<leader>fs', function()
+      --   require('telescope.builtin').live_grep({
+      --     search_dirs = { vim.fn.expand('%:p') },
+      --   })
+      -- end, {})
       vim.keymap.set('n', '<leader>ps', function()
         builtin.grep_string({ search = vim.fn.input("grep > ") })
       end)
