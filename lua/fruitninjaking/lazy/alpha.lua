33ddc1e66acba3dec598635ec74a565ff9ac8c4b
wesleytruong <wesleytruong@ucsb.edu>
2024-11-20 01:00:24 -0800

    added better symbols to dashboard

--- lua/fruitninjaking/lazy/alpha.lua
+++ lua/fruitninjaking/lazy/alpha.lua
@@ -13,12 +13,12 @@ return {
       dashboard.section.header.val = ascii['anime']
 
       dashboard.section.buttons.val = {
-        dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+        dashboard.button("f", "󰺄  Find file", ":Telescope find_files <CR>"),
         dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-        dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-        dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+        dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+        dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
         dashboard.button("c", "  Configuration", ":cd ~/.config/nvim | Explore<CR>"),
-        dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+        dashboard.button("q", "󰈆  Quit Neovim", ":qa<CR>"),
       }
 
       require('alpha').setup(dashboard.opts)
