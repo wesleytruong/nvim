3c73dc3f4c0486b3a99e365fbf5ec6311e12ce25
wesleytruong <wesleytruong@ucsb.edu>
2024-11-20 11:21:32 -0800

    added ignore diagnostic for lua vim

--- lua/fruitninjaking/lazy/lsp.lua
+++ lua/fruitninjaking/lazy/lsp.lua
@@ -20,7 +20,15 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lsp = require("lspconfig")
-      lsp.lua_ls.setup({})
+      lsp.lua_ls.setup({
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = {'vim'}
+            }
+          }
+        }
+      })
       lsp.clangd.setup({})
       lsp.pyright.setup({})
       lsp.gopls.setup({})
