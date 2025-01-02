c2e411ef1c40371e94a8886bcbfb14de32711b6b
wesleytruong <wesleytruong@ucsb.edu>
2025-01-01 18:23:07 -0800

    switched lsp to use blink.cmp

--- lua/fruitninjaking/lazy/lsp.lua
+++ lua/fruitninjaking/lazy/lsp.lua
@@ -18,27 +18,58 @@ return {
   {
     -- allows to set up keybinds to navigate files with lang server features
     "neovim/nvim-lspconfig",
-    config = function()
-      local lsp = require("lspconfig")
-      lsp.lua_ls.setup({
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = {'vim'}
+    dependencies = { 'saghen/blink.cmp' },
+    opts = {
+      servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = {'vim'}
+              }
             }
           }
-        }
-      })
-      lsp.clangd.setup({})
-      lsp.pyright.setup({})
-      lsp.gopls.setup({})
-      -- lsp.jdtls.setup({})
+        },
+        clangd = {},
+        pyright = {},
+        gopls = {},
+      }
+    },
+    config = function(_, opts)
+      local lsp = require("lspconfig")
+      for server, config in pairs(opts.servers) do
+        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+        lsp[server].setup(config)
+      end
 
       vim.keymap.set('n','K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n', 'td', vim.lsp.buf.type_definition, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
     end
+  },
+  -- {
+  --   'hrsh7th/nvim-cmp',
+  --   dependencies = {
+  --     'hrsh7th/cmp-nvim-lsp',
+  --     -- "zbirenbaum/copilot-cmp",
+  --   },
+  --   opts = {
+  --     sources = {
+  --       -- { name = 'copilot' },
+  --       { name = 'nvim_lsp' },
+  --     },
+  --   }
+  -- }
+  {
+    'saghen/blink.cmp',
+    version = '*',
+    opts = {
+      keymap = { preset = 'default' },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+    },
   }
 }
 --[[return {
