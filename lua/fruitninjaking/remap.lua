1729062dcb765a6d33cff3b445187afdac5414b0
wesleytruong <wesleytruong@ucsb.edu>
2025-01-16 12:59:10 -0800

    changed file explore to oil

--- lua/fruitninjaking/remap.lua
+++ lua/fruitninjaking/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Explore)
 
 -- when in highlight mode, can move text around with J and K
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
