aedd4438115cd309bbf74e7e05b4dec954cb7491
wesleytruong <wesleytruong@ucsb.edu>
2025-01-16 13:00:30 -0800

    removed markdown override
    markdown override was changing the tab spacing from 2 to 4

--- lua/fruitninjaking/set.lua
+++ lua/fruitninjaking/set.lua
@@ -36,3 +36,5 @@ vim.opt.breakindent = true
 -- indent by an additional 2 characters on wrapped lines, when line >= 40 characters, put 'showbreak' at start of line
 vim.opt.breakindentopt = "shift:2,min:40,sbr"
 -- vim.opt.showbreak = ">>"
+
+vim.g.markdown_recommended_style = 0
