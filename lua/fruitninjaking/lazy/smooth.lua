ddb563a18542f40634c16c02fd3e9ae31a2af359
wesleytruong <wesleytruong@ucsb.edu>
2025-01-16 12:58:39 -0800

    added smooth scrolling

--- lua/fruitninjaking/lazy/smooth.lua
+++ lua/fruitninjaking/lazy/smooth.lua
@@ -1,9 +1,17 @@
 return {
-  "sphamba/smear-cursor.nvim",
-  opts = {
-    -- smear_between_neighbor_lines = false,
-    stiffness = 1,
-    trailing_stiffness = 0.3,
-    legacy_computing_symbols_support = true, -- removes shadow for transparent bgs
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      -- smear_between_neighbor_lines = false,
+      stiffness = 1,
+      trailing_stiffness = 0.3,
+      legacy_computing_symbols_support = true, -- removes shadow for transparent bgs
+    },
   },
+  {
+    "karb94/neoscroll.nvim",
+    opts = {
+      duration_multiplier = 0.3,
+    },
+  }
 }
